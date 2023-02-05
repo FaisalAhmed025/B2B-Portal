8e9941c152159c4bfef9640485a63c9a1d8ea721
Md Faisal Ahmed <md.faisal3@northsouth.edu>
2023-02-05 18:45:18 +0600

    file upload module implmented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,22 @@
 import { JwtAuthGuard } from './auth/Guard/jwt-auth.guard';
 import { AuthService } from 'src/auth/auth.service';
-import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/guard/local-auth.guard';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller()
 export class AppController {
@@ -12,13 +25,32 @@ export class AppController {
     private authservice: AuthService,
   ) {}
 
-  // @Post('auth/login')
-  // async login(@Request() req) {
-  //   console.log();
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file',
+  {
+    storage:diskStorage({
+      destination: './files',
+      filename:(req, file, callback)=>{
+        const uniqueSuffix = Date.now() + '-' +Math.round(Math.random()*1e9);
+        const ext = extname(file.originalname)
+        const filename = `${file.originalname}-${uniqueSuffix}${ext}`;
+        callback(null, filename)
+      }
+    })
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log("uploaded file is :", file);
     
-  //   return this.authservice.login(req.body);
-  // }
-  // @UseGuards(JwtAuthGuard)
+    return 'file uploaded  ';
+  }
+
+  @Post('auth/login')
+  async login(@Request() req) {
+    console.log();
+
+    return this.authservice.login(req.body);
+  }
+  @UseGuards(JwtAuthGuard)
   @Get('Homepage')
   getHello(): string {
     return this.appService.getHello();
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
   // @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req);
+    return this.authService.login(req.body);
     return req.user;
   }
 
